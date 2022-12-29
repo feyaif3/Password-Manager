4ef710e84ed9d24169c92352564915dab91e710b
frankeyaife <frankeyaife@yahoo.co.uk>
2022-12-29 22:36:45 +0000

    Update password-manager.py

--- password-manager.py
+++ password-manager.py
@@ -3,20 +3,21 @@ from tkinter import messagebox
 from random import choice, randint, shuffle
 #------------Password Generator------------#
 import random
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '@', '-', '*', '+','?','£','.',',',]
+def create_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '@', '-', '*', '+','?','£','.',',',]
 
-password_letters = [random.choice(letters) for _ in range(randint(8,10))] #choose random list of letters in range for 8-10
-password_numbers = [random.choice(numbers) for _ in range(randint(3,5))]
-password_symbols = [random.choice(symbols) for _ in range(randint(2,4))]
+    password_letters = [random.choice(letters) for _ in range(randint(8,10))] #choose random list of letters in range for 8-10
+    password_numbers = [random.choice(numbers) for _ in range(randint(3,5))]
+    password_symbols = [random.choice(symbols) for _ in range(randint(2,4))]
 
-password_list = password_letters + password_numbers + password_symbols
-shuffle(password_list)
+    password_list = password_letters + password_numbers + password_symbols
+    shuffle(password_list)
 
-password = "".join(password_list) #all characters will join together to create the password
+    password = "".join(password_list) #all characters will join together to create the password
 
-print(f"Your password is: {password}")
+    print(f"Your password is: {password}")
 #--------------Save Password---------------#
 
 def add():
